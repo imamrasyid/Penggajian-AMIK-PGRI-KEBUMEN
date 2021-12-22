4ec94c48a8b7d6e1a68e313848d6139670a4543d
EyeTracker <imamrasyid@gmail.com>
2021-12-22 18:31:59 +0700

    Delete Unused File

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -9,6 +9,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 Class Logout extends CI_Controller
 {
+    function __construct()
+    {
+        parent::__construct();
+        // Write Code Here
+    }
+    
     function index()
     {
         $this->session->sess_destroy();
